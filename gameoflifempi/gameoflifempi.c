402382e028c1f81a13daf5da2f2f721316628992
denniswiebe <dennis_wiebe98@yahoo.de>
2021-03-05 13:51:53 +0100

    MPI first exercise

--- gameoflifempi/gameoflifempi.c
+++ gameoflifempi/gameoflifempi.c
@@ -1,13 +1,15 @@
-#include "mpi.h"
+#include <openmpi/mpi.h>
 #include <stdio.h>
 
 int main(int argc, char* argv[]) {
-    int rank, size, i;
-    // MPI_STATUS status;
+    int rank, size, i;    
 
+    MPI_Status status;
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    printf("size %d, rank %d\n", size, rank);
 
     MPI_Finalize();
     return 0;
